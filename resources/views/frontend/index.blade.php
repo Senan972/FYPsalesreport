affa1794a75ee55c71229a7fdc6a4abb9e0a1686
Naqi <inaqi69@gmail.com>
2022-12-28 00:08:33 +0500

    add to cart added to all index page

--- resources/views/frontend/index.blade.php
+++ resources/views/frontend/index.blade.php
@@ -552,13 +552,13 @@ HealthMart
 
 
      <button class="btn btn-primary icon" type="button" title="Add Cart" data-toggle="modal" data-target="#exampleModal" id="{{ $product->id }}" onclick="productView(this.id)"> <i class="fa fa-shopping-cart"></i> </button>
-  
-  <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
-</li>
+        
+        <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
+      </li>
 
+      
 
-
-  <button class="btn btn-primary icon" type="button" title="Wishlist" id="{{ $product->id }}" onclick="addToWishList(this.id)"> <i class="fa fa-heart"></i> </button>
+        <button class="btn btn-primary icon" type="button" title="Wishlist" id="{{ $product->id }}" onclick="addToWishList(this.id)"> <i class="fa fa-heart"></i> </button>
 
         <li class="lnk"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal" aria-hidden="true"></i> </a> </li>
       </ul>
@@ -636,15 +636,15 @@ $discount = ($amount/$product->selling_price) * 100;
     <ul class="list-unstyled">
       <li class="add-cart-button btn-group">
         
-     <button class="btn btn-primary icon" type="button" title="Add Cart" data-toggle="modal" data-target="#exampleModal" id="{{ $product->id }}" onclick="productView(this.id)"> <i class="fa fa-shopping-cart"></i> </button>
-  
-     <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
-   </li>
-   
-   
-   
-     <button class="btn btn-primary icon" type="button" title="Wishlist" id="{{ $product->id }}" onclick="addToWishList(this.id)"> <i class="fa fa-heart"></i> </button>
-   
+        <button class="btn btn-primary icon" type="button" title="Add Cart" data-toggle="modal" data-target="#exampleModal" id="{{ $product->id }}" onclick="productView(this.id)"> <i class="fa fa-shopping-cart"></i> </button>
+        
+        <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
+      </li>
+
+      
+
+        <button class="btn btn-primary icon" type="button" title="Wishlist" id="{{ $product->id }}" onclick="addToWishList(this.id)"> <i class="fa fa-heart"></i> </button>
+
            <li class="lnk"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal" aria-hidden="true"></i> </a> </li>
          </ul>
   </div>
@@ -722,15 +722,15 @@ $discount = ($amount/$product->selling_price) * 100;
     <ul class="list-unstyled">
       <li class="add-cart-button btn-group">
         
-     <button class="btn btn-primary icon" type="button" title="Add Cart" data-toggle="modal" data-target="#exampleModal" id="{{ $product->id }}" onclick="productView(this.id)"> <i class="fa fa-shopping-cart"></i> </button>
-  
-     <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
-   </li>
-   
-   
-   
-     <button class="btn btn-primary icon" type="button" title="Wishlist" id="{{ $product->id }}" onclick="addToWishList(this.id)"> <i class="fa fa-heart"></i> </button>
-   
+    <button class="btn btn-primary icon" type="button" title="Add Cart" data-toggle="modal" data-target="#exampleModal" id="{{ $product->id }}" onclick="productView(this.id)"> <i class="fa fa-shopping-cart"></i> </button>
+        
+        <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
+      </li>
+
+      
+
+        <button class="btn btn-primary icon" type="button" title="Wishlist" id="{{ $product->id }}" onclick="addToWishList(this.id)"> <i class="fa fa-heart"></i> </button>
+
            <li class="lnk"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal" aria-hidden="true"></i> </a> </li>
          </ul>
   </div>
@@ -839,14 +839,14 @@ $discount = ($amount/$product->selling_price) * 100;
       <li class="add-cart-button btn-group">
         
      <button class="btn btn-primary icon" type="button" title="Add Cart" data-toggle="modal" data-target="#exampleModal" id="{{ $product->id }}" onclick="productView(this.id)"> <i class="fa fa-shopping-cart"></i> </button>
-  
-     <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
-   </li>
-   
-   
-   
-     <button class="btn btn-primary icon" type="button" title="Wishlist" id="{{ $product->id }}" onclick="addToWishList(this.id)"> <i class="fa fa-heart"></i> </button>
-   
+        
+        <button class="btn btn-primary cart-btn" type="button">Add to cart</button>
+      </li>
+
+      
+
+        <button class="btn btn-primary icon" type="button" title="Wishlist" id="{{ $product->id }}" onclick="addToWishList(this.id)"> <i class="fa fa-heart"></i> </button>
+
            <li class="lnk"> <a data-toggle="tooltip" class="add-to-cart" href="detail.html" title="Compare"> <i class="fa fa-signal" aria-hidden="true"></i> </a> </li>
          </ul>
   </div>
@@ -1185,7 +1185,6 @@ $discount = ($amount/$product->selling_price) * 100;
                     <div class="rating rateit-small"></div>
                     <div class="description">Medicines & Devices, Wah Cant</div>
                     <!-- /.product-price --> 
-                    
                   </div>
                   <!-- /.product-info -->
                   <div class="cart clearfix animate-effect">
