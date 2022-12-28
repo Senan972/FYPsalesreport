bd57f801137c127327689634c0ad97386311ff0f
Naqi <inaqi69@gmail.com>
2022-12-28 19:13:15 +0500

    fixes

--- app/Http/Controllers/User/AllUserController.php
+++ app/Http/Controllers/User/AllUserController.php
@@ -11,7 +11,7 @@ use App\Models\OrderItem;
 use Illuminate\Support\Facades\Session;
 use Auth;
 use Carbon\Carbon;
-
+ 
 use App\Mail\OrderMail;
 use PDF;
 
@@ -34,21 +34,24 @@ class AllUserController extends Controller
 
     } // end mehtod 
 
+
+
     public function InvoiceDownload($order_id){
-        $order = Order::with('division','district','state','user')->where('id',$order_id)->where('user_id',Auth::id())->first();
-         $orderItem = OrderItem::with('product')->where('order_id',$order_id)->orderBy('id','DESC')->get();
-         // return view('frontend.user.order.order_invoice',compact('order','orderItem'));
+       $order = Order::with('division','district','state','user')->where('id',$order_id)->where('user_id',Auth::id())->first();
+    	$orderItem = OrderItem::with('product')->where('order_id',$order_id)->orderBy('id','DESC')->get();
+    	// return view('frontend.user.order.order_invoice',compact('order','orderItem'));
 		$pdf = PDF::loadView('frontend.user.order.order_invoice',compact('order','orderItem'))->setPaper('a4')->setOptions([
-            'tempDir' => public_path(),
-            'chroot' => public_path(),
-    ]);
-    return $pdf->download('invoice.pdf');
- 
- 
- 
-     } // end mehtod 
- 
-     public function ReturnOrder(Request $request,$order_id){
+				'tempDir' => public_path(),
+				'chroot' => public_path(),
+		]);
+		return $pdf->download('invoice.pdf');
+
+
+
+    } // end mehtod 
+
+
+    public function ReturnOrder(Request $request,$order_id){
 
         Order::findOrFail($order_id)->update([
             'return_date' => Carbon::now()->format('d F Y'),
@@ -66,12 +69,16 @@ class AllUserController extends Controller
 
     } // end method 
 
+
+
     public function ReturnOrderList(){
 
         $orders = Order::where('user_id',Auth::id())->where('return_reason','!=',NULL)->orderBy('id','DESC')->get();
         return view('frontend.user.order.return_order_view',compact('orders'));
 
     } // end method 
+
+
 
     public function CancelOrders(){
 
@@ -80,38 +87,36 @@ class AllUserController extends Controller
 
     } // end method 
 
-  ///////////// Order Traking ///////
 
-  public function OrderTraking(){
 
-  } // end mehtod 
+    ///////////// Order Traking ///////
 
- ///////////// Order Traking ///////
+    public function OrderTraking(Request $request){
 
- public function OrderTraking(Request $request){
+        $invoice = $request->code;
 
-    $invoice = $request->code;
+        $track = Order::where('invoice_no',$invoice)->first();
 
-    $track = Order::where('invoice_no',$invoice)->first();
+        if ($track) {
+            
+            // echo "<pre>";
+            // print_r($track);
 
-    if ($track) {
+        return view('frontend.traking.track_order',compact('track'));
 
-        // echo "<pre>";
-        // print_r($track);
+        }else{
 
-    return view('frontend.traking.track_order',compact('track'));
+            $notification = array(
+            'message' => 'Invoice Code Is Invalid',
+            'alert-type' => 'error'
+        );
 
-    }else{
+        return redirect()->back()->with($notification);
 
-        $notification = array(
-        'message' => 'Invoice Code Is Invalid',
-        'alert-type' => 'error'
-    );
+        }
 
-    return redirect()->back()->with($notification);
+    } // end mehtod 
 
-    }
- } // end mehtod 
 
 
 
--- resources/views/frontend/user/order/order_view.blade.php
+++ resources/views/frontend/user/order/order_view.blade.php
@@ -5,25 +5,26 @@
 	<div class="container">
 		<div class="row">
 			 @include('frontend.common.user_sidebar')
-             <div class="col-md-2">
+
+       <div class="col-md-2">
        </div>
 
-       <div class="col-md-8">
+       <div class="col-md-10">
 
         <div class="table-responsive">
           <table class="table">
             <tbody>
-
+  
               <tr style="background: #e2e2e2;">
-                <td class="col-md-1">
+                <td class="col-md-2">
                   <label for=""> Date</label>
                 </td>
 
-                <td class="col-md-3">
+                <td class="col-md-2">
                   <label for=""> Total</label>
                 </td>
 
-                <td class="col-md-3">
+                <td class="col-md-2">
                   <label for=""> Payment</label>
                 </td>
 
@@ -36,10 +37,10 @@
                   <label for=""> Order</label>
                 </td>
 
-                 <td class="col-md-1">
+                 <td class="col-md-2">
                   <label for=""> Action </label>
                 </td>
-
+                
               </tr>
 
 
@@ -50,7 +51,7 @@
                 </td>
 
                 <td class="col-md-3">
-                  <label for=""> ${{ $order->amount }}</label>
+                  <label for=""> Rs. {{ $order->amount }}</label>
                 </td>
 
 
@@ -58,17 +59,19 @@
                   <label for=""> {{ $order->payment_method }}</label>
                 </td>
 
-                 <td class="col-md-2">
+                <td class="col-md-2">
+                  <label for=""> {{ $order->invoice_no }}</label>
+                </td>
+
+         <td class="col-md-2">
           <label for=""> 
 
     @if($order->status == 'pending')        
         <span class="badge badge-pill badge-warning" style="background: #800080;"> Pending </span>
         @elseif($order->status == 'confirm')
          <span class="badge badge-pill badge-warning" style="background: #0000FF;"> Confirm </span>
-                    
-         
 
-         @elseif($order->status == 'processing')
+          @elseif($order->status == 'processing')
          <span class="badge badge-pill badge-warning" style="background: #FFA500;"> Processing </span>
 
           @elseif($order->status == 'picked')
@@ -80,7 +83,7 @@
           @elseif($order->status == 'delivered')
          <span class="badge badge-pill badge-warning" style="background: #008000;"> Delivered </span>
 
-         @if($order->return_order == 1) 
+          @if($order->return_order == 1) 
            <span class="badge badge-pill badge-warning" style="background:red;">Return Requested </span>
 
           @endif
@@ -93,15 +96,12 @@
         </td>
 
          <td class="col-md-1">
-         <a href="{{ url('user/order_details/'.$order->id ) }}" class="btn btn-sm btn-primary"><i class="fa fa-eye"></i> View</a>
+          <a href="{{ url('user/order_details/'.$order->id ) }}" class="btn btn-sm btn-primary"><i class="fa fa-eye"></i> View</a>
 
-
-         <a target="_blank" href="{{ url('user/invoice_download/'.$order->id ) }}" class="btn btn-sm btn-danger" style="margin-top: 5px;"><i class="fa fa-download" style="color: white;"></i> Invoice </a>
-
-
-
+           <a target="_blank" href="{{ url('user/invoice_download/'.$order->id ) }}" class="btn btn-sm btn-danger" style="margin-top: 5px;"><i class="fa fa-download" style="color: white;"></i> Invoice </a>
+          
         </td>
-
+                
               </tr>
               @endforeach
 
@@ -110,25 +110,26 @@
 
 
             </tbody>
-
+            
           </table>
-
+          
         </div>
 
 
 
 
-
+         
        </div> <!-- / end col md 8 -->
 
+		 
 
-
-
+		 
+			
 		</div> <!-- // end row -->
-
+		
 	</div>
-
+	
 </div>
-
+ 
 
 @endsection
